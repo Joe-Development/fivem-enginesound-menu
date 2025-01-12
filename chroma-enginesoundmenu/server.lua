7cc8824b6f9187edb4f2d3dc2c90ba35c52ee8f3
Grav <75702884+Gravxd@users.noreply.github.com>
2025-01-12 01:02:20 +0000

    tweak(server) remove manual version checks, switch to ox_lib + delete version.txt

--- chroma-enginesoundmenu/server.lua
+++ chroma-enginesoundmenu/server.lua
@@ -35,24 +35,8 @@ CreateThread(function()
         return
     end
 
+    lib.versionCheck('Gravxd/enginesound-menu')
+    
+    print(string.format("^6chroma-enginesoundmenu ^2has been loaded - enjoy!\n^2For support or issues, please visit ^3https://discord.gg/chromalabs^7"))
 
-    -- https://github.com/Blumlaut/FiveM-Resource-Version-Check-Thing/
-    local updatePath = "/Gravxd/enginesound-menu"       -- your git user/repo path
-    local resourceName = "^6chroma-enginesoundmenu"     -- the resource name
-
-    local function checkVersion(err, responseText, headers)
-        local curVersion = LoadResourceFile(GetCurrentResourceName(), "version.txt")     -- make sure the "version" file actually exists in your resource root!
-        if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
-            print("\n" ..
-            resourceName ..
-            " ^1is outdated, please update it from:\n^3https://github.com/Gravxd/enginesound-menu/releases/latest\n^1For support or issues, please visit ^3https://discord.gg/chromalabs^7")
-        else
-            print("\n" ..
-            resourceName ..
-            " ^2is up to date, and has been loaded - enjoy!\nFor support or issues, please visit ^3https://discord.gg/chromalabs^7")
-        end
-    end
-
-    PerformHttpRequest("https://raw.githubusercontent.com" .. updatePath .. "/main/chroma-enginesoundmenu/version.txt",
-        checkVersion, "GET")
 end)
