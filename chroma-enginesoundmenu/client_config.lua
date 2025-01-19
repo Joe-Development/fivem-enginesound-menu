51331ec1cd417ac82d321226e5c52e480110e7f2
Grav <75702884+Gravxd@users.noreply.github.com>
2025-01-19 22:19:05 +0000

    tweak(client_config) remove closeOnSelect (un-used)

--- chroma-enginesoundmenu/client_config.lua
+++ chroma-enginesoundmenu/client_config.lua
@@ -1,7 +1,6 @@
 Config = {
     Keybind = "",                  -- E.G F7 ---> https://docs.fivem.net/docs/game-references/controls/
     MenuPosition = "bottom-right", -- bottom-right, bottom-left, top-right, top-left
-    CloseOnSelect = false,         -- Will close the menu when you select an engine sound.
     StoreSoundsByModel = true,    -- This option only applies as a default setting, do you want to (by default) store engine sounds by the model (spawncode)
     Notify = function(msg, type)
         -- customise this notification function to whatever you desire - by default it uses ox_lib but you can edit this
